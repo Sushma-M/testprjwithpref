f2db6750baba4305038b9f6350cc50248baee34e
root <root@7edff035593d>
2016-10-20 12:52:56 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "testprjwithpref",
+  "homePage" : "Main",
+  "name" : "testprjwithpref",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
